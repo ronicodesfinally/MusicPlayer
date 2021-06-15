7463c0732543e5ecbc71e5927a5cd9cb12010594
Roni <talk2roni@gmail.com>
2021-06-15 17:23:30 +0530

    Initial Commit

---
+++ GenreButtonScreen.swift
@@ -0,0 +1,29 @@
+//
+//  GenreButtonScreen.swift
+//  
+//
+//  Created by Roni on 11/06/21.
+//
+
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MusicPlayer/GenreButtonScreen.swift
@@ -0,0 +1,42 @@
+import UIKit
+import MediaPlayer
+
+class GenreButtonScreen: UIViewController {
+
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func genrebuttontapped(_ sender: UIButton) {
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized{
+                self.playGenre(genre: sender.currentTitle!)
+            }
+        }
+    }
+    
+    @IBAction func Stopbuttontapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+
+    @IBAction func NextButtonTapped(_ sender: Any) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String) {
+        
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+    }
+}
